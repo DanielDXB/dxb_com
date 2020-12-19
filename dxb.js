f43950a1b4cd464119b718e07e219a8d6899e142
DanielDXB <75695058+DanielDXB@users.noreply.github.com>
2020-12-19 15:13:42 +0200

    Add files via upload

---
+++ dxb.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'photos')))
+app.get('/' , (req , res) => {
+    res.sendFile(path.join(__dirname+'/index.html'))
+})
+app.get('/about' , (req , res) => {
+    res.sendFile(path.join(__dirname+'/aboutme.html'))
+})
+app.get('/mywork' , (req , res) => {
+    res.sendFile(path.join(__dirname+'/mywork.html'))
+})
+app.get('/contact' , (req , res) => {
+    res.sendFile(path.join(__dirname+'/contact.html'))
+})
+app.listen(port)
+
+
+
