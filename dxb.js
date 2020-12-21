a7124dde447d6cf432159faa6dd2649ed74460a5
DanielDXB <75695058+DanielDXB@users.noreply.github.com>
2020-12-21 23:07:52 +0200

    Update dxb.js

--- dxb.js
+++ dxb.js
@@ -16,7 +16,7 @@ app.get('/mywork' , (req , res) => {
 app.get('/contact' , (req , res) => {
     res.sendFile(path.join(__dirname+'/contact.html'))
 })
-app.listen(port)
+app.listen(port || process.env.port)
 
 
 
